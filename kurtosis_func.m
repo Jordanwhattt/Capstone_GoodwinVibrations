72e7102731b1394a72cf91fe0652dc04aa9d5329
Jordanwhattt <114823165+Jordanwhattt@users.noreply.github.com>
2023-06-09 16:05:09 -0400

    Update kurtosis_func.m

--- kurtosis_func.m
+++ kurtosis_func.m
@@ -1,4 +1,4 @@
-
+% This files calcualtes the kurtosis of a window. n is the number of samples in a window and x is the data.
 function kurtosis = kurtosis_func(n, x) % Calculates Kurtosis
     num_sum = sum((x-mean(x)).^4);
     den_sum = sum((x - mean(x)).^2);
